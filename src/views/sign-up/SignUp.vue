9658a83d03d821540a03d92d909fa6ca71f7839e
Basar Buyukkahraman <basarbk@yahoo.com>
2024-01-09 22:14:10 +0300

    0202-interactions

--- src/views/sign-up/SignUp.vue
+++ src/views/sign-up/SignUp.vue
@@ -10,11 +10,49 @@
   </div>
   <div>
     <label for="password">Password</label>
-    <input id="password" type="password" />
+    <input id="password" type="password" @input="onChangePassword" />
   </div>
   <div>
     <label for="passwordRepeat">Password Repeat</label>
-    <input id="passwordRepeat" type="password" />
+    <input id="passwordRepeat" type="password" @input="onChangePasswordRepeat" />
   </div>
-  <button disabled>Sign Up</button>
+  <button :disabled="isDisabled">Sign Up</button>
 </template>
+<script setup>
+import { ref } from 'vue'
+
+const isDisabled = ref(true)
+const password = ref()
+const passwordRepeat = ref()
+
+const onChangePassword = (event) => {
+  password.value = event.target.value
+  isDisabled.value = password.value !== passwordRepeat.value
+}
+
+const onChangePasswordRepeat = (event) => {
+  passwordRepeat.value = event.target.value
+  isDisabled.value = password.value !== passwordRepeat.value
+}
+</script>
+<!-- <script>
+export default {
+  data() {
+    return {
+      isDisabled: true,
+      password: undefined,
+      passwordRepeat: undefined
+    }
+  },
+  methods: {
+    onChangePassword(event) {
+      this.password = event.target.value
+      this.isDisabled = this.password !== this.passwordRepeat
+    },
+    onChangePasswordRepeat(event) {
+      this.passwordRepeat = event.target.value
+      this.isDisabled = this.password !== this.passwordRepeat
+    }
+  }
+}
+</script> -->
