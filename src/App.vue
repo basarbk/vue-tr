065255fc9da9e212cddc140c3178ac8a499a658c
Basar Buyukkahraman <basarbk@yahoo.com>
2024-01-13 21:53:20 +0300

    0502-activation-page

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,8 @@ import NavBar from './components/NavBar.vue'
 <template>
   <NavBar />
   <main class="container mt-3">
+    <router-link class="d-block" to="/activation/123">Activation 123</router-link>
+    <router-link class="d-block" to="/activation/456">Activation 456</router-link>
     <RouterView />
     <LanguageSelector />
   </main>
---
+++ src/views/activation/Activation.vue
@@ -0,0 +1,21 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <h1>Activation</h1>
+</template>
+<script setup>
+import axios from 'axios'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  axios.patch('/api/v1/users/token-from-url/active')
+})
+</script>
+<!-- <script>
+import axios from 'axios'
+
+export default {
+  mounted() {
+    axios.patch('/api/v1/users/token-from-url/active')
+  }
+}
+</script> -->
