e37387cc5d85fa3726e3d4b5102bbfc5fffb7489
Basar Buyukkahraman <basarbk@yahoo.com>
2024-01-13 22:29:05 +0300

    0504-handle-api-req-res

--- src/views/activation/Activation.vue
+++ src/views/activation/Activation.vue
@@ -1,27 +1,77 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <h1>Activation</h1>
+  <div>
+    <span v-if="status === 'loading'" class="spinner-border" aria-hidden="true"></span>
+    <div v-if="status === 'fail'" class="alert alert-danger" role="alert">
+      {{ errorMessage }}
+    </div>
+    <div v-if="status === 'success'" class="alert alert-success" role="alert">
+      {{ successMessage }}
+    </div>
+  </div>
 </template>
 <script setup>
 import axios from 'axios'
-import { watchEffect } from 'vue'
+import { watchEffect, ref } from 'vue'
+import { useI18n } from 'vue-i18n'
 import { useRoute } from 'vue-router'
+const { t } = useI18n()
 const route = useRoute()
 
-watchEffect(() => {
-  axios.patch(`/api/v1/users/${route.params.token}/active`)
+const errorMessage = ref()
+const successMessage = ref()
+const status = ref()
+
+watchEffect(async () => {
+  status.value = 'loading'
+  try {
+    const response = await axios.patch(`/api/v1/users/${route.params.token}/active`)
+    successMessage.value = response.data.message
+    status.value = 'success'
+  } catch (apiError) {
+    if (apiError.response?.data?.message) {
+      errorMessage.value = apiError.response.data.message
+    } else {
+      errorMessage.value = t('genericError')
+    }
+    status.value = 'fail'
+  }
 })
 </script>
 <!-- <script>
 import axios from 'axios'
 
 export default {
+  data() {
+    return {
+      status: undefined,
+      errorMessage: undefined,
+      successMessage: undefined
+    }
+  },
   mounted() {
-    axios.patch(`/api/v1/users/${this.$route.params.token}/active`)
+    this.sendActivationRequest()
+  },
+  methods: {
+    async sendActivationRequest() {
+      this.status = 'loading'
+      try {
+        const response = await axios.patch(`/api/v1/users/${this.$route.params.token}/active`)
+        this.successMessage = response.data.message
+        this.status = 'success'
+      } catch (apiError) {
+        if (apiError.response?.data?.message) {
+          this.errorMessage = apiError.response.data.message
+        } else {
+          this.errorMessage = this.$t('genericError')
+        }
+        this.status = 'fail'
+      }
+    }
   },
   watch: {
     '$route.params.token'() {
-      axios.patch(`/api/v1/users/${this.$route.params.token}/active`)
+      this.sendActivationRequest()
     }
   }
 }
