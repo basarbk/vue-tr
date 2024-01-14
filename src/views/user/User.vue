e931b952d2fc6fe554f8a79a50a1d759b37b6123
Basar Buyukkahraman <basarbk@yahoo.com>
2024-01-14 23:18:52 +0300

    0707-user-page

--- src/views/user/User.vue
+++ src/views/user/User.vue
@@ -1,4 +1,40 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <h1>User Page</h1>
+  <div>
+    <AppAlert variant="danger" v-if="status === 'fail'">{{ errorMessage }}</AppAlert>
+    <AppAlert variant="secondary" center v-if="status === 'loading'">
+      <AppSpinner size="normal" />
+    </AppAlert>
+    <ProfileCard v-if="status === 'success'" :user="user" />
+  </div>
 </template>
+<script setup>
+import ProfileCard from './components/ProfileCard.vue'
+import { useRoute } from 'vue-router'
+import getUserById from './api'
+import { ref, watchEffect } from 'vue'
+import { useI18n } from 'vue-i18n'
+import AppSpinner from '@/components/AppSpinner.vue'
+import AppAlert from '@/components/AppAlert.vue'
+
+const { t } = useI18n()
+const route = useRoute()
+const errorMessage = ref('')
+const status = ref()
+const user = ref()
+watchEffect(async () => {
+  status.value = 'loading'
+  try {
+    const response = await getUserById(route.params.id)
+    user.value = response.data
+    status.value = 'success'
+  } catch (apiError) {
+    status.value = 'fail'
+    if (apiError.response?.data?.message) {
+      errorMessage.value = apiError.response.data.message
+    } else {
+      errorMessage.value = t('genericError')
+    }
+  }
+})
+</script>
---
+++ src/views/user/components/ProfileCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <AppCard>
+    <template v-slot:header>
+      <img
+        class="rounded-circle shadow"
+        width="200"
+        height="200"
+        :alt="user.username + ' profile'"
+        src="/assets/profile.png"
+      />
+    </template>
+    <template v-slot:body>
+      <div class="text-center">
+        <h3>{{ user.username }}</h3>
+      </div>
+    </template>
+  </AppCard>
+</template>
+<script setup>
+import AppCard from '@/components/AppCard.vue'
+defineProps({
+  user: Object
+})
+</script>
