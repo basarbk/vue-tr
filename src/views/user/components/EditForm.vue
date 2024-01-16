5b814e358bef04a9c5d7e6ea8114abeec514dc02
Basar Buyukkahraman <basarbk@yahoo.com>
2024-01-16 18:23:56 +0300

    0806-5-handle-failure

--- src/views/user/components/EditForm.vue
+++ src/views/user/components/EditForm.vue
@@ -1,6 +1,7 @@
 <template>
   <form @submit.prevent="submit">
-    <AppInput id="username" :label="$t('username')" v-model="username" />
+    <AppInput id="username" :label="$t('username')" v-model="username" :help="errors.username" />
+    <AppAlert v-if="error" variant="danger">{{ error }}</AppAlert>
     <AppButton type="submit" :api-progress="apiProgress">{{ $t('save') }}</AppButton>
     <div class="d-inline m-1"></div>
     <AppButton type="button" variant="outline-secondary" @click="$emit('cancel')">{{
@@ -11,20 +12,40 @@
 <script setup>
 import AppButton from '@/components/AppButton.vue'
 import AppInput from '@/components/AppInput.vue'
+import AppAlert from '@/components/AppAlert.vue'
 import { useAuthStore } from '@/stores/auth'
 import { updateUser } from './api'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
+import { useI18n } from 'vue-i18n'
 
 const emit = defineEmits(['cancel', 'save'])
 
+const { t } = useI18n()
 const { auth, update } = useAuthStore()
+const error = ref()
+const errors = ref({})
 const apiProgress = ref(false)
 const username = ref(auth.username)
-
 const submit = async () => {
   apiProgress.value = true
-  await updateUser(auth.id, { username: username.value })
-  update({ username: username.value })
-  emit('save')
+  error.value = undefined
+  try {
+    await updateUser(auth.id, { username: username.value })
+    update({ username: username.value })
+    emit('save')
+  } catch (apiError) {
+    apiProgress.value = false
+    if (apiError.response?.status === 400) {
+      errors.value = apiError.response.data.validationErrors
+    } else {
+      error.value = t('genericError')
+    }
+  }
 }
+watch(
+  () => username.value,
+  () => {
+    delete errors.value.username
+  }
+)
 </script>
